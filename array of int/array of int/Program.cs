5f5cb36c6818be7e6e883d59e515edce0304a508
Adina Pop <147972913+PopAdina@users.noreply.github.com>
2024-06-03 18:16:06 +0300

    Add files via upload

---
+++ array of int/array of int/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_of_int
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inventory = { 200, 450, 700, 175, 250 };
+            int sum = 0;
+            int bin = 0;
+            foreach (int items in inventory)
+            {
+                sum += items;
+                bin++;
+                Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+            }
+            Console.WriteLine($"We have {sum} items in inventory.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ code block/code block/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code_block
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            int value = 0;
+
+            if (flag)
+            {
+                value = 10;
+                Console.WriteLine($"Inside of code block: {value}");
+            }
+                Console.WriteLine($"Outside of code block: {value}");
+                Console.ReadKey();
+            }
+        }
+    }
+
+
