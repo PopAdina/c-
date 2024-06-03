a40ed6f53f0fc6d0b0ea9c784e2e67f9da11e6ac
Adina Pop <147972913+PopAdina@users.noreply.github.com>
2024-06-03 18:21:35 +0300

    Add files via upload

---
+++ zar/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zar 
+            Random dice = new Random();
+            int roll = dice.Next(1, 7);
+            Console.WriteLine(roll);
+            Console.ReadLine();
+       
+
+
+            int number = 7;
+            string text = "seven";
+
+            Console.WriteLine(number);
+            Console.WriteLine();
+            Console.WriteLine(text);
+            Console.ReadLine();
+
+
+            Random dice1 = new Random();
+            int roll1 = dice.Next();
+            int roll2 = dice.Next(101);
+            int roll3 = dice.Next(50, 101);
+
+            Console.WriteLine($"First roll: {roll1}");
+            Console.WriteLine($"Second roll: {roll2}");
+            Console.WriteLine($"Third roll: {roll3}");
+            Console.ReadKey();
+        }
+    }
+}
