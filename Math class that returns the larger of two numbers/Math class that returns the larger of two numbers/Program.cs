db8256d8468f58fe01aa929ca87dfd7a55374fef
Adina Pop <147972913+PopAdina@users.noreply.github.com>
2024-06-03 18:18:47 +0300

    Add files via upload

---
+++ Math class that returns the larger of two numbers/Math class that returns the larger of two numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_class_that_returns_the_larger_of_two_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstValue = 500;
+            int secondValue = 600;
+            int largerValue;
+            largerValue = Math.Max(firstValue, secondValue);
+
+            Console.WriteLine(largerValue);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Project 2 code/Project 2 code/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string readResult;
+            string roleName = "";
+            bool validEntry = false;
+
+            do
+            {
+                Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+                readResult = Console.ReadLine();
+                if (readResult != null)
+                {
+                    roleName = readResult.Trim();
+                }
+
+                if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user")
+                {
+                    validEntry = true;
+                }
+                else
+                {
+                    Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
+                }
+
+            } while (validEntry == false);
+
+            Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+            readResult = Console.ReadLine();
+        }
+    }
+}
